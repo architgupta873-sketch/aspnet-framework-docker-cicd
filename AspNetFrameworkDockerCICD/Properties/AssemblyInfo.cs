3ca4ecbae89edf88d0739b9fbee9c87a7783d6a8
archit's mac <architsmac@archits-MacBook-Air-2.local>
2026-05-20 10:33:15 +0530

    Initial ASP.NET Framework project

---
+++ AspNetFrameworkDockerCICD/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AspNetFrameworkDockerCICD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AspNetFrameworkDockerCICD")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4d5bee35-4e83-490c-a6ae-e6b3980dc91b")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
